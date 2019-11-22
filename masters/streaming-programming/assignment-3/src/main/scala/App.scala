b15bc926f215b9f309aedac8b3ee9b7618bc3cee
Alexander Onbysh <alexander.onbysh@gmail.com>
2019-11-22 15:28:30 +0200

    stream-programming: assignment-3 - add speed tests

--- masters/streaming-programming/assignment-3/src/main/scala/App.scala
+++ masters/streaming-programming/assignment-3/src/main/scala/App.scala
@@ -5,17 +5,52 @@ import scala.io.Source
  */
 
 object App extends App {
+  def time[R](block: => R, text: String = "Run test", rounds: Int = 10): Unit = {
+
+    var times: Long = 0
+    for (_ <- 1 to rounds) {
+      val t0 = System.nanoTime()
+      val result = block
+      times += System.nanoTime() - t0
+    }
+    println(s"$text: " + times / rounds * 0.000001 + " ms")
+  }
+
   val employees_source = Source.fromResource("employees.csv").getLines
   val departments_source = Source.fromResource("departments.csv").getLines
 
   val employees = Parser.parseEmployees(employees_source)
   val departments = Parser.parseDepartments(departments_source)
 
-  val analytics: Analytics = new NonParallelAnalytics(employees, departments)
+  val nonParallel: Analytics = new NonParallelAnalytics(employees, departments)
+  val parallel: Analytics = new NonParallelAnalytics(employees, departments)
 
-  println(employees)
-  println(analytics.EmployeesForManagers)
-  println(analytics.EmployeeWithLowestSalaryInEachDepartment)
-  println(analytics.EmployeesWithSalaryOfDepartmentHigherThen(5000))
-  println(analytics.EmployeesWhoHiredBeforeManager)
+
+  time({
+    nonParallel.EmployeesForManagers
+  }, text = "NonParallel EmployeesForManagers")
+  time({
+    parallel.EmployeesForManagers
+  }, text = "Parallel EmployeesForManagers")
+  println("---")
+  time({
+    nonParallel.EmployeeWithLowestSalaryInEachDepartment
+  }, text = "NonParallel EmployeeWithLowestSalaryInEachDepartment")
+  time({
+    parallel.EmployeeWithLowestSalaryInEachDepartment
+  }, text = "Parallel EmployeeWithLowestSalaryInEachDepartment")
+  println("---")
+  time({
+    nonParallel.EmployeesWithSalaryOfDepartmentHigherThen(10000)
+  }, text = "NonParallel EmployeesWithSalaryOfDepartmentHigherThen")
+  time({
+    parallel.EmployeesWithSalaryOfDepartmentHigherThen(10000)
+  }, text = "Parallel EmployeesWithSalaryOfDepartmentHigherThen")
+  println("---")
+  time({
+    nonParallel.EmployeesForManagers
+  }, text = "NonParallel EmployeesForManagers")
+  time({
+    parallel.EmployeesForManagers
+  }, text = "Parallel EmployeesForManagers")
 }
