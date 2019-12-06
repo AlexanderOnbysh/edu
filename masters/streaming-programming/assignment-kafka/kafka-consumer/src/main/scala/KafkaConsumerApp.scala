381db0f49cecb5404f6f1f99751061ed787d9bee
Alexander Onbysh <alexander.onbysh@gmail.com>
2019-12-06 22:32:48 +0200

    kafka-assignment: ex9

--- masters/streaming-programming/assignment-kafka/kafka-consumer/src/main/scala/KafkaConsumerApp.scala
+++ masters/streaming-programming/assignment-kafka/kafka-consumer/src/main/scala/KafkaConsumerApp.scala
@@ -1,6 +1,40 @@
 /**
  * Created by Alexander Onbysh 06.12.2019
  */
-object KafkaConsumerApp {
+
+import java.util.Properties
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+
+import scala.collection.JavaConverters._
+
+object KafkaConsumerApp extends App {
+  val props = new Properties()
+  props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094")
+  props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+  props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-group")
+
+  val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
+
+  try {
+    consumer.subscribe(List("task9").asJava)
+    while (true) {
+      val records = consumer.poll(10)
+      for (record <- records.asScala) {
+        println("Topic: " + record.topic() +
+          ",Key: " + record.key() +
+          ",Value: " + record.value() +
+          ", Offset: " + record.offset() +
+          ", Partition: " + record.partition())
+      }
+    }
+  } catch {
+    case e: Exception => e.printStackTrace()
+  }
+  finally {
+    consumer.close()
+  }
 
 }
